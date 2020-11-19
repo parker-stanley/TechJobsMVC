62257f2dcb3ad6df9ed0ac458962ef22101ac4ca
Parker Stanley <parker.stanley@SOMO-M-DC-005.fios-router.home>
2020-11-19 17:47:23 -0500

    not the cleanest but all tasks finished and the bonus mission of retaining user selection of radio button post-search

--- TechJobsMVC/Controllers/SearchController.cs
+++ TechJobsMVC/Controllers/SearchController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TechJobsMVC.Data;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,6 +18,23 @@ namespace TechJobsMVC.Controllers
             return View();
         }
 
-        // TODO #3: Create an action method to process a search request and render the updated search view. 
+        public IActionResult Result(string searchType, string searchTerm)
+        {
+            ViewBag.columns = ListController.ColumnChoices;
+            ViewBag.searchType = searchType;
+
+            if (String.IsNullOrEmpty(searchTerm))
+            {
+                ViewBag.jobs = JobData.FindAll();
+
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+
+                return View("Index");
+            }
+        }
     }
 }
